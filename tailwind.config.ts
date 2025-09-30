ba5a3a0d78cf0a68e018412cb11374029b592a5a
Douglas Sanchez <38049028+dougdesigner@users.noreply.github.com>
2025-09-30 11:49:25 -0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import formsPlugin from "@tailwindcss/forms"
 import type { Config } from "tailwindcss"
 
 const config: Config = {
-  darkMode: "selector",
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
