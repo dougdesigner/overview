e566413c3494aa44b4ca963bca0323409bbed04d
Douglas Sanchez <38049028+dougdesigner@users.noreply.github.com>
2025-09-30 11:22:08 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/",
-        destination: "/support",
+        destination: "/overview",
         permanent: true,
       },
     ]
